da13c32d45f25f892f540ed4069ddfa7fcf835fb
Andrew Lavers <andrew.lavers@radialpoint.com>
2014-09-21 18:25:19 -0400

    Errors as a package

---
+++ packages/alavers:errors/package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  summary: "A pattern to display application errors to the user",
+  version: "1.0.0"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1.1');
+  api.use(['minimongo', 'mongo-livedata', 'templating', 'waitingkuo:jade'], 'client');
+  api.addFiles(['errors.js', 'errors_list.jade', 'errors_list.js'], 'client');
+
+  if (api.export) {
+    api.export('Errors');
+  }
+});
+
+// Package.onTest(function(api) {
+//   api.use('tinytest');
+//   api.use('alavers:errors');
+//   api.addFiles('alavers:errors-tests.js');
+// });
