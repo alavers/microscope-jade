1b093494101399949a0ae75530fe3f7ed5fcfa1e
Andrew Lavers <andrew.lavers@radialpoint.com>
2014-10-05 13:28:36 -0400

    Pagination cursor

--- server/publications.js
+++ server/publications.js
@@ -1,11 +1,15 @@
-Meteor.publish('posts', function() {
-    return Posts.find();
+Meteor.publish('posts', function(options) {
+    return Posts.find({}, options);
 });
 
 Meteor.publish('comments', function(postId) {
-  return Comments.find({postId: postId});
+    return Comments.find({
+        postId: postId
+    });
 });
 
 Meteor.publish('notifications', function() {
-  return Notifications.find({userId: this.userId});
+    return Notifications.find({
+        userId: this.userId
+    });
 });
