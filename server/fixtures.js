129e0680ff519d631d1605ff45efc2d0ec61c854
Andrew Lavers <andrew.lavers@radialpoint.com>
2014-10-10 19:55:05 -0400

    basic upvote ui and method

--- server/fixtures.js
+++ server/fixtures.js
@@ -23,7 +23,9 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: now - 7 * 3600 & 1000,
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [],
+        votes: 0
     });
 
     Comments.insert({
@@ -48,7 +50,9 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://meteor.com',
         submitted: now - 10 * 3600 * 1000,
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     Posts.insert({
@@ -57,7 +61,9 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
         submitted: now - 12 * 3600 * 1000,
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     for (var i = 0; i < 10; i++) {
@@ -67,7 +73,9 @@ if (Posts.find().count() === 0) {
             author: tom.profile.name,
             url: 'http://google.com?q=test-' + i,
             submitted: now - i * 3600 * 1000,
-            commentsCount: 0
+            commentsCount: 0,
+            upvoters: [],
+            votes: 0
         });
     }
 }
