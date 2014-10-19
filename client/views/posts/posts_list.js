ae4fe58daf05f923832ed3ccf5fa2542655b34fe
Andrew Lavers <andrew.lavers@radialpoint.com>
2014-10-18 20:19:37 -0400

    animation

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,1 +1,9 @@
-Template.postsList.helpers({});
+Template.postsList.helpers({
+    postsWithRank: function() {
+        this.posts.rewind();
+        return this.posts.map(function(post, index, cursor) {
+            post._rank = index;
+            return post;
+        });
+    }
+});
